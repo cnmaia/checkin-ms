8778aa31944fabfcc00ef4c909141034bcdc7dcf
Caio Maia <caaiomaia@gmail.com>
2016-09-29 00:06:37 -0300

    First Blood

---
+++ src/main/java/br/mack/Application.java
@@ -0,0 +1,15 @@
+package br.mack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by cmaia on 29/09/16
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
