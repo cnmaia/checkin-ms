066c8b52cc48b3355622bdfe3fd493981bf57010
Caio Maia <caaiomaia@gmail.com>
2016-09-29 01:15:18 -0300

    Handle validation error

--- src/main/java/br/mack/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/br/mack/controller/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package br.mack.controller.handler;
 
 import br.mack.exception.ResourceNotFoundException;
+import br.mack.exception.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
 
 /**
  * Created by cmaia on 29/09/16
@@ -15,5 +18,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity handleResourceNotFound(ResourceNotFoundException ex) {
         return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity handleValidationException(ValidationException ex) {
+        return new ResponseEntity<Map<String, String>>(ex.getValidationErrors(), HttpStatus.BAD_REQUEST);
     }
 }
